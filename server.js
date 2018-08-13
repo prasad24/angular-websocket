24bb82ca71ddfa953eeb969d313c34ffc9624a1b
Prasad Subramanian <prasad2@hotmail.com>
2018-08-13 18:19:33 -0400

    Added client side html and configured socket.io on server

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+
 const app = express();
+
+//Socket IO setup
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
 const PORT = process.env.PORT || 3000;
 
+io.on('connection', socket => {
+    console.log('New connection has been made');
+})
 
-app.listen(PORT, ()=>{
+http.listen(PORT, ()=>{
     console.log('Listening on port:' + PORT);
 })
 
